b8eff46c2a16e64d719b8d68ad0597424fb05df5
Cocytee <56483083+Cocytee@users.noreply.github.com>
2019-10-17 14:06:10 +0200

    Add files via upload

---
+++ CamoCore.py
@@ -0,0 +1,26 @@
+# learning base and statistical work
+# Egien values, avergage stats, pop stats and Dicriminant Analysis
+
+#need multivariate stats , numpy, stats and opencv package
+
+#imageInfo
+	# process & disp (image)
+	# process & disp (FFT).(r,g,b)
+
+#terrainInfo
+	# process & disp (AvegrageImage)
+	# process & disp (AvegrageFFT)
+
+#SaveTerrainStats
+	# save (AverageFFT + FFT_Array)
+
+#GlobalEigen
+	# process (AllTerrainEigenValue)
+	# save (Eigen)
+
+#GlobalDisplay
+	# process & disp DA of all TerrainStats
+
+#CamoGen
+	#Use Eigen to generate Camo 
+	#Transform at the good resolution
---
+++ Session.py
@@ -0,0 +1,10 @@
+# create a session storing the camo and the current setting values
+# interacting with GUI
+# camocore init possible
+
+#display console and lauching camo_init
+
+#Session Class 
+	#storing Setting Classes and CamoImg
+	#try / tests
+	#running things
