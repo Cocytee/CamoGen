ad98518e2327b357cd31f90e4aa5ff81c627af5e
Cocytee <56483083+Cocytee@users.noreply.github.com>
2019-10-18 15:14:37 +0200

    Useless stuffs

--- CamoCore.py
+++ CamoCore.py
@@ -1,28 +1,31 @@
 # learning base and statistical work
 # Egien values, avergage stats, pop stats and Dicriminant Analysis
 
-#need multivariate stats , numpy, stats and opencv package
-import cv2 as ocv
+#need numpy, sklearn, matplotlib and opencv package
+import cv2 as cv
 import numpy as np
 import sklearn as skl
 import matplotlib.pyplot as plt
 
 class TerrainStat:
-    def __init__(self):
-    	self.Size = 0
+	def __init__(self):
+		self.Size = 0
 		self.Average = []
 		self.Set = []
-	def setSize(self,nb)
+	def setSize(self,nb):
 		self.Size = nb
-	def addSet(self,Stat)
+	def addSet(self,Stat):
 		self.Set.append(Stat)
-	def setAvg(self)
+	def setAvg(self):
 		self.Average = mean(self.Set)
 
-def imageInfo(file,disp=False)
-	#open file
-	#process it
-	#display it if needed
+def imageInfo(file,disp=False):
+	img = cv.imread(file,0)
+	dft = cv.dft(np.float32(img))
+	if disp==True :
+		plt.imshow(dft, cmap = 'gray')
+		plt.show
+	return dft
 
 #terrainInfo
 	# process & disp (AvegrageImage)
--- Session.py
+++ Session.py
@@ -4,14 +4,17 @@
 
 #display console and lauching camo_init
 
+from CamoCore import *
+
 #Session Class 
 	#storing Setting Classes and CamoImg
 	#try / tests
 	#running things
 class Session:
-    def __init__(self, pset, cset, sset, cc):
+	def __init__(self, pset, cset, sset, cc):
 		self.ProcSetting = pset
 		self.CamoSetting = cset
 		self.SaveSetting = sset
 		self.CamoCurrent = cc
 
+test = imageInfo('LearningBase/BUSHES/BUSH1.jpg',disp=True)
